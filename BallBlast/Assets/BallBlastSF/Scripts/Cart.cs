667c292e08005d095205cf19ab1b47bb5e467de0
Pyotr <pyotrk@inbox.lv>
2024-10-31 22:49:00 +0200

    first_commit

---
+++ BallBlast/Assets/BallBlastSF/Scripts/Cart.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cart : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float vehicleWidth;
+
+    [Header("Wheels")]
+    [SerializeField] private Transform[] wheels;
+    [SerializeField] private float wheelRadius;
+
+
+    private Vector3 movementTarget;
+    private float deltaMovement;
+    private float lastPositionX;
+
+
+    private void Start()
+    {
+        movementTarget = transform.position;
+    }
+
+    private void Update()
+    {
+        Move();
+
+        RotateWheel();
+    }
+
+    private void Move()
+    {
+        lastPositionX = transform.position.x;
+
+        transform.position = Vector3.MoveTowards(transform.position, movementTarget, movementSpeed * Time.deltaTime);
+
+        deltaMovement = transform.position.x - lastPositionX;
+    }
+
+    public void SetMovementTarget(Vector3 target)
+    {
+        movementTarget = ClampMovementTarget(target);
+    }
+
+    private void RotateWheel()
+    {
+        float angle = (180 * deltaMovement) / (Mathf.PI * wheelRadius * 2);
+
+        for (int i = 0; i < wheels.Length; i++)
+        {
+            wheels[i].Rotate(0, 0, -angle);
+        }
+    }
+
+    private Vector3 ClampMovementTarget(Vector3 target)
+    {
+        float leftBorder = -8.8f + vehicleWidth * 0.5f;
+        float rightBorder = 8.8f - vehicleWidth * 0.5f;
+
+        Vector3 movTarget = target;
+        movTarget.z = transform.position.z;
+        movTarget.y = transform.position.y;
+
+        if (movTarget.x < leftBorder) movTarget.x = leftBorder;
+        if (movTarget.x > rightBorder) movTarget.x = rightBorder;
+
+        return movTarget;
+    }
+
+
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawLine(transform.position - new Vector3(vehicleWidth * 0.5f, 0.5f, 0), transform.position + new Vector3(vehicleWidth * 0.5f, -0.5f, 0));
+    }
+#endif
+
+}
---
+++ BallBlast/Assets/BallBlastSF/Scripts/Turret.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    [SerializeField] private Projectile projectilePrefab;
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private float fireRate;
+    [SerializeField] public int projectileAmount;
+    [SerializeField] private float projectileInterval;
+
+
+    private float timer;
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+    }
+
+    private void SpawnProjecttile()
+    {
+        float startPosX = shootPoint.position.x - projectileInterval * (projectileAmount - 1) * 0.5f;
+
+        for (int i = 0; i < projectileAmount; i++)
+        {
+            Instantiate(projectilePrefab, new Vector3(startPosX + i * projectileInterval, shootPoint.position.y, shootPoint.position.z), transform.rotation);
+        }
+
+        
+    }
+
+    public void Fire()
+    {
+        if (timer >= fireRate)
+        {
+            SpawnProjecttile();
+            timer = 0;
+        }
+    }
+
+}
