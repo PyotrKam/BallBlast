6d5d0b7e9adbcdd7cfc6e81e6e325c8e56d1ec3a
Pyotr <pyotrk@inbox.lv>
2024-11-05 22:07:15 +0200

    Spawn with error
    IndexOutOfRangeException: Index was outside the bounds of the array.
    StoneSpawner.Spawn () (at Assets/BallBlastSF/Scripts/StoneSpawner.cs:65)
    StoneSpawner.Update () (at Assets/BallBlastSF/Scripts/StoneSpawner.cs:45)

---
+++ BallBlast/Assets/BallBlastSF/Scripts/StoneSpawner.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StoneSpawner : MonoBehaviour
+{
+    [Header("Spawn")]
+    [SerializeField] private Stone stonePrefab;
+    [SerializeField] private Transform[] spawnPoints;
+    [SerializeField] private float spawnRate;
+
+    [Header("Balance")]
+    [SerializeField] private Turret turret;
+    [SerializeField] public int amount;
+    [SerializeField][Range(0.0f, 1.0f)] private float minHitpointsPercentage;
+    [SerializeField] public float maxHitpointsRate;
+
+    [Header("Colors")]
+    [SerializeField] private Color[] colors; 
+
+    [Space(10)] public UnityEvent Completed;
+
+    private float timer;
+    private float amountSpawned;
+    private int stoneMaxHitpoints;
+    private int stoneMinHitpoints;
+
+    private void Start()
+    {
+        //int damagePerSecond = (int)((turret.Damage * turret.ProjectileAmount) * (1 / turret.FireRate));
+
+        //stoneMaxHitpoints = (int)(damagePerSecond * maxHitpointsRate);
+        stoneMinHitpoints = (int)(stoneMaxHitpoints * minHitpointsPercentage);
+
+        timer = spawnRate;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= spawnRate)
+        {
+            Spawn();
+
+            timer = 0;
+        }
+
+        if (amountSpawned == amount)
+        {
+            enabled = false;
+
+            Completed.Invoke();
+        }
+    }
+
+    private void Spawn()
+    {
+        Stone stone = Instantiate(stonePrefab, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+        stone.SetSize((Stone.Size)Random.Range(1, 4));
+        stone.maxHitPoints = Random.Range(stoneMinHitpoints, stoneMaxHitpoints + 1);
+
+        
+        Color randomColor = colors[Random.Range(0, colors.Length)];
+        randomColor.a = 1f; 
+        stone.SetColor(randomColor);
+
+        amountSpawned++;
+    }
+}
