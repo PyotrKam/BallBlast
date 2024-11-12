4d49421d3f7fc66891cc42a9afbd433e612e6cb7
Pyotr <pyotrk@inbox.lv>
2024-11-12 18:48:55 +0200

    Starting with coins
    Add coin script

---
+++ BallBlast/Assets/BallBlastSF/Scripts/Coin.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private float fallspeed;
+    [SerializeField] private float minY;
+
+    private void Update()
+    {
+        if (transform.position.y > minY)
+        {
+            transform.position += Vector3.down * fallspeed * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Cart cart = collision.GetComponent<Cart>();
+
+        if (cart != null)
+        {
+            //cart.CollectCoin();
+            Destroy(gameObject);
+        }
+
+    }
+
+}
